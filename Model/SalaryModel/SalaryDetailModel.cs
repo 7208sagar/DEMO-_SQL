70acdc908255f6d38a39ff36562094dce8d3e59e
sagar <7208sk@gmail.com>
2021-11-11 22:36:27 +0530

    PayRoll

---
+++ Model/SalaryModel/SalaryDetailModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagment.Model.SalaryModel
+{
+    class SalaryDetailModel
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+       public string JobDiscription { get; set; }
+        public string Month { get; set; }
+        public int EmployeeSalary { get; set; }
+        public int Salary { get; set; }
+    }
+}
